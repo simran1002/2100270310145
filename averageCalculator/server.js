6fb9c078cf5f5d98959d459f63d0d116cb943a76
Simran_Yadav <simranyadav464@gmail.com>
2024-06-08 13:51:47 +0530

    Updated the code

--- averageCalculator/server.js
+++ averageCalculator/server.js
@@ -1,11 +1,14 @@
+require("dotenv").config();
+
 const express = require('express');
 const axios = require('axios');
 
 const app = express();
 const PORT = 9876;
 const WINDOW_SIZE = 10;
-const testServerUrl = 'http://20.244.56.144/test';
-const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzE3ODM0MDA0LCJpYXQiOjE3MTc4MzM3MDQsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6IjkwYjIxMzY5LWM0ZTktNDUxNC1iNzRmLWYwODJlZjk1MzBlZiIsInN1YiI6InNpbXJhbnlhZGF2NDY0QGdtYWlsLmNvbSJ9LCJjb21wYW55TmFtZSI6IkFqYXkgS3VtYXIgR2FyZyBFbmdpbmVlcmluZyBDb2xsZWdlIiwiY2xpZW50SUQiOiI5MGIyMTM2OS1jNGU5LTQ1MTQtYjc0Zi1mMDgyZWY5NTMwZWYiLCJjbGllbnRTZWNyZXQiOiJuTEdWTXFpeWpIaXdwTWZoIiwib3duZXJOYW1lIjoiU2ltcmFuIFlhZGF2Iiwib3duZXJFbWFpbCI6InNpbXJhbnlhZGF2NDY0QGdtYWlsLmNvbSIsInJvbGxObyI6IjIxMDAyNzAzMTAxNDUifQ.k_feCzN60iJ51YRVqQBNwBtXTGf-VeFo778YIWQx7uc'
+const testServerUrl = process.env.TEST_URL;
+const AUTH_TOKEN = process.env.AUTH_TOKEN;
+
 app.get('/numbers/:numberid', async (req, res) => {
   const { numberid } = req.params;
   const urlMap = {
@@ -33,7 +36,7 @@ app.get('/numbers/:numberid', async (req, res) => {
 
     const fetchedNumbers = response.data.numbers;
     
-    console.log(`Received numbers: ${JSON.stringify(fetchedNumbers)}`);
+    console.log(`Received numbers: ${fetchedNumbers.join(', ')}`);
 
     if (!Array.isArray(fetchedNumbers)) {
       return res.status(500).json({ error: 'Invalid response from test server' });
@@ -41,15 +44,12 @@ app.get('/numbers/:numberid', async (req, res) => {
 
     const uniqueNumbers = Array.from(new Set(fetchedNumbers));
 
-    // Calculate previous window and current window if necessary
-    // Use uniqueNumbers directly if it represents the current window
-
     const average = uniqueNumbers.reduce((acc, num) => acc + num, 0) / uniqueNumbers.length;
 
     res.json({
       numbers: uniqueNumbers,
-      windowPrevState: [], // Replace with your actual previous state data
-      windowCurrState: uniqueNumbers, // Assuming uniqueNumbers represent the current window
+      windowPrevState: [], 
+      windowCurrState: uniqueNumbers, 
       avg: parseFloat(average.toFixed(2)),
     });
   } catch (error) {
